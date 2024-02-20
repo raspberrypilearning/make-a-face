f9047778ed4894916c29d24978c6b038719f3fce
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-02-20 17:44:05 +0000

    New translations main.py (Italian)

--- it-IT/code/make_a_face_interactive_example/main.py
+++ it-IT/code/make_a_face_interactive_example/main.py
@@ -17,46 +17,46 @@ def happy(x_middle, y_eye, y_mouth):
 
 
 def setup():
-    # Put code to run once here
-    size(400, 400)  # width and height
-    background(0, 0, 0)  # move under draw() to reset the drawing every frame
+    # Metti qui sotto il codice che verrà eseguito una sola volta
+    size(400, 400)  # larghezza e altezza
+    background(0, 0, 0) # spostati sotto draw() per ripristinare il disegno ad ogni fotogramma
     rect_mode(CENTER)
     no_stroke()
 
 
 def draw():
-    # Put code to run every frame here
+    # Metti qui sotto il codice che verrà eseguito ad ogni cambio di frame
     mask_width = width / 2
     x_middle = width / 2
     y_eye = 180
     y_mouth = 255
-    # draw mask
-    fill(255, 255, 255)  # white
+    # disegna la maschera
+    fill(255, 255, 255)  # bianco
     rect(200, 150, mask_width, mask_width)
     ellipse(x_middle, 250, mask_width, 140)
-    # eyes and mouth
-    fill(0)  # black
-    ellipse(x_middle - 50, y_eye, 60, 50)  # x, y, width, height
+    # occhi e bocca
+    fill(0)  # nero
+    ellipse(x_middle - 50, y_eye, 60, 50)  # x, y, larghezza, altezza
     ellipse(x_middle + 50, y_eye, 60, 50)
     ellipse(x_middle, y_mouth, 100, 75)
-    # partly cover eyes and mouth
+    # coprire parzialmente occhi e bocca
     fill(255)
     if mouse_x > x_middle:
         happy(x_middle, y_eye, y_mouth)
     else:
         sad(x_middle, y_eye, y_mouth)
-    # cover top of mask
+    # coprire la parte superiore della maschera
     fill(0)
     ellipse(x_middle, 60, 250, 90)
-    # shade half of the mask
+    # ombreggiare metà della maschera
     fill(0, 25)
     rect(300, 200, width/2, height)
 
 
 def mouse_pressed():
-    # Put code to run when the mouse is pressed here
+    # Inserisci qui il codice da eseguire quando si preme il mouse
     print(mouse_x, mouse_y)
 
 
-# Keep this to run your code
+# Conserva questa parte per eseguire il codice
 run()
