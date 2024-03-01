8250907b087247c13e13d26c4bf7fcaaf57187fd
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-01 12:43:14 +0000

    New translations main.py (Dutch)

--- nl-NL/code/make_a_face_stacked_faces_example/main.py
+++ nl-NL/code/make_a_face_stacked_faces_example/main.py
@@ -10,88 +10,88 @@ def setup():
 
 
 def draw():
-    blue = Color(92, 204, 206)
-    green = Color(149, 212, 122)
-    red = Color(239, 62, 91)
-    purple = Color(75, 37, 109)
-    brown = Color(178, 162, 150)
-    grey = Color(201, 201, 201)
-    lilac = Color(160, 158, 214)
+    blauw = Color(92, 204, 206)
+    groen = Color(149, 212, 122)
+    rood = Color(239, 62, 91)
+    paars = Color(75, 37, 109)
+    bruin = Color(178, 162, 150)
+    grijs = Color(201, 201, 201)
+    lila = Color(160, 158, 214)
 
-    # Top face background
-    fill(blue)
+    # Bovenste gezicht achtergrond
+    fill(blauw)
     rect(50, 100, 300, 200)
     fill(0)
 
-    # Top face Hair
-    fill(purple)
-    gap = 0
+    # Haar van het bovenste gezicht
+    fill(paars)
+    tussenruimte = 0
     for i in range(0, 5):
-        triangle(100+gap, 140, 120+gap, 120, 140+gap, 140)
-        gap = gap+40
+        triangle(100+ tussenruimte, 140, 120+ tussenruimte, 120, 140+ tussenruimte, 140)
+        tussenruimte = tussenruimte+40
 
-    # Top face Left Eye
-    fill(grey)
+    # Bovenste gezicht linkeroog
+    fill(grijs)
     rect(80, 190, 100, 50)
-    fill(red)
+    fill(rood)
     triangle(190, 250, 70, 150, 180, 160)
-    fill(green)
+    fill(groen)
     triangle(190, 250, 60, 160, 180, 170)
-    fill(lilac)
+    fill(lila)
     ellipse(160, 200, 30, 30)
 
-    # Top face Right Eye
-    fill(grey)
+    # Bovenste gezicht rechteroog
+    fill(grijs)
     rect(220, 190, 100, 50)
-    fill(red)
+    fill(rood)
     triangle(210, 250, 330, 150, 220, 160)
-    fill(green)
+    fill(groen)
     triangle(210, 250, 340, 160, 220, 170)
-    fill(lilac)
+    fill(lila)
     ellipse(240, 200, 30, 30)
 
-    # Top face Mouth
-    fill(brown)
+    # Bovenste gezicht mond
+    fill(bruin)
     rect(100, 220, 200, 50)
-    fill(purple)
+    fill(paars)
     rect(110, 240, 180, 10)
 
-  # Bottom face background
-    fill(purple)
+  # Achtergrond van het onderste gezicht
+    fill(paars)
     rect(50, 300, 300, 200)
     fill(0)
 
-    # Bottom face Hair
-    fill(green)
-    gap = 0
+    # Haar van het onderste gezicht
+    fill(groen)
+    tussenruimte = 0
     for i in range(0, 5):
-        triangle(100+gap, 340, 120+gap, 320, 140+gap, 340)
-        gap = gap+40
+        triangle(100+tussenruimte, 340, 120+tussenruimte, 320, 140+tussenruimte, 340)
+        tussenruimte = tussenruimte+40
 
-    # Bottom face Left Eye
-    fill(red)
+    # Onderste gezicht linkeroog
+    fill(rood)
     rect(80, 390, 100, 50)
-    fill(lilac)
+    fill(lila)
     triangle(190, 450, 70, 350, 180, 360)
-    fill(brown)
+    fill(bruin)
     triangle(190, 450, 60, 360, 180, 370)
-    fill(purple)
+    fill(paars)
     ellipse(160, 400, 30, 30)
 
-    # Bottom face Right Eye
-    fill(red)
+    # Onderste gezicht rechter oog
+    fill(rood)
     rect(220, 390, 100, 50)
-    fill(lilac)
+    fill(lila)
     triangle(210, 450, 330, 350, 220, 360)
-    fill(brown)
+    fill(bruin)
     triangle(210, 450, 340, 360, 220, 370)
-    fill(purple)
+    fill(paars)
     ellipse(240, 400, 30, 30)
 
-    # Bottom face Mouth
-    fill(green)
+    # Onderste gezicht mond
+    fill(groen)
     rect(100, 420, 200, 50)
-    fill(red)
+    fill(rood)
     rect(110, 440, 180, 10)
 
 
