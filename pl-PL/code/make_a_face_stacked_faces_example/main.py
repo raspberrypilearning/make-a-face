eed98af168ef0c710088d2c04dd6c367fa8bbe59
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-15 13:12:56 +0000

    New translations main.py (Polish)

--- pl-PL/code/make_a_face_stacked_faces_example/main.py
+++ pl-PL/code/make_a_face_stacked_faces_example/main.py
@@ -10,88 +10,88 @@ def setup():
 
 
 def draw():
-    blue = Color(92, 204, 206)
-    green = Color(149, 212, 122)
-    red = Color(239, 62, 91)
-    purple = Color(75, 37, 109)
-    brown = Color(178, 162, 150)
-    grey = Color(201, 201, 201)
-    lilac = Color(160, 158, 214)
+    Niebieski = Kolor(92, 204, 206)
+    Zielony = Kolor(149, 212, 122)
+    Czerwony = Kolor(239, 62, 91)
+    Fioletowy = Kolor(75, 37, 109)
+    Brązowy = Kolor(178, 162, 150)
+    Szary = Kolor(201, 201, 201)
+    Liliowy = Kolor(160, 158, 214)
 
-    # Top face background
-    fill(blue)
+    # Tło górnej twarzy
+    fill(niebieski)
     rect(50, 100, 300, 200)
     fill(0)
 
-    # Top face Hair
-    fill(purple)
-    gap = 0
+    # Włosy na wierzchu
+    fill(fioletowy)
+    przerwa = 0
     for i in range(0, 5):
-        triangle(100+gap, 140, 120+gap, 120, 140+gap, 140)
-        gap = gap+40
+        trójkąt(100+gap, 140, 120+gap, 120, 140+gap, 140)
+        przerwa = przerwa+40
 
-    # Top face Left Eye
-    fill(grey)
+    # Górna twarz Lewe oko
+    fill(szary)
     rect(80, 190, 100, 50)
-    fill(red)
+    fill(czerwony)
     triangle(190, 250, 70, 150, 180, 160)
-    fill(green)
+    fill(zielony)
     triangle(190, 250, 60, 160, 180, 170)
-    fill(lilac)
+    fill(liliowy)
     ellipse(160, 200, 30, 30)
 
-    # Top face Right Eye
-    fill(grey)
+    # Górna twarz prawego oka
+    fill(szary)
     rect(220, 190, 100, 50)
-    fill(red)
+    fill(czerwony)
     triangle(210, 250, 330, 150, 220, 160)
-    fill(green)
+    fill(zielony)
     triangle(210, 250, 340, 160, 220, 170)
-    fill(lilac)
+    fill(liliowy)
     ellipse(240, 200, 30, 30)
 
-    # Top face Mouth
-    fill(brown)
+    # Usta górnej twarzy
+    fill(brązowy)
     rect(100, 220, 200, 50)
-    fill(purple)
+    fill(fioletowy)
     rect(110, 240, 180, 10)
 
-  # Bottom face background
-    fill(purple)
+  # Dolna twarz tła
+    fill(fioletowy)
     rect(50, 300, 300, 200)
     fill(0)
 
-    # Bottom face Hair
-    fill(green)
-    gap = 0
+    # Dolna twarz włosy
+    fill(zielony)
+    przerwa = 0
     for i in range(0, 5):
-        triangle(100+gap, 340, 120+gap, 320, 140+gap, 340)
-        gap = gap+40
+        trójkąt(100+gap, 340, 120+gap, 320, 140+gap, 340)
+        przerwa = przerwa+40
 
-    # Bottom face Left Eye
-    fill(red)
+    # Dolna ściana Lewe oko
+    fill(czerwony)
     rect(80, 390, 100, 50)
-    fill(lilac)
+    fill(liliowy)
     triangle(190, 450, 70, 350, 180, 360)
-    fill(brown)
+    fill(brązowy)
     triangle(190, 450, 60, 360, 180, 370)
-    fill(purple)
+    fill(fioletowy)
     ellipse(160, 400, 30, 30)
 
-    # Bottom face Right Eye
-    fill(red)
+    # Dolna twarz prawego oka
+    fill(czerwony)
     rect(220, 390, 100, 50)
-    fill(lilac)
+    fill(liliowy)
     triangle(210, 450, 330, 350, 220, 360)
-    fill(brown)
+    fill(brązowy)
     triangle(210, 450, 340, 360, 220, 370)
-    fill(purple)
+    fill(fioletowy)
     ellipse(240, 400, 30, 30)
 
-    # Bottom face Mouth
-    fill(green)
+    # Usta dolnej twarzy
+    fill(zielony)
     rect(100, 420, 200, 50)
-    fill(red)
+    fill(czerwony)
     rect(110, 440, 180, 10)
 
 
