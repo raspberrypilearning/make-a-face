ccc24c2da63d20882a01a888d2b966208104dfe7
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-15 13:12:20 +0000

    New translations main.py (Polish)

--- pl-PL/code/make_a_face_interactive_example/main.py
+++ pl-PL/code/make_a_face_interactive_example/main.py
@@ -5,58 +5,58 @@ from random import randint
 
 
 def sad(x_middle, y_eye, y_mouth):
-    ellipse(x_middle - 50, y_eye - 20, 60, 50)  # x, y, width, height
-    ellipse(x_middle + 50, y_eye - 20, 60, 50)
-    ellipse(x_middle, y_mouth + 30, 100, 65)
+    elipsa(x_middle - 50, y_eye - 20, 60, 50) # x, y, szerokość, wysokość
+    elipsa(x_middle + 50, y_eye - 20, 60, 50)
+    elipsa(x_middle, y_mouth + 30, 100, 65)
 
 
-def happy(x_middle, y_eye, y_mouth):
-    ellipse(x_middle - 50, y_eye + 20, 60, 50)  # x, y, width, height
-    ellipse(x_middle + 50, y_eye + 20, 60, 50)
-    ellipse(x_middle, y_mouth - 30, 100, 65)
+def szczęśliwy(x_middle, y_eye, y_mouth):
+    elipsa(x_middle - 50, y_eye + 20, 60, 50) # x, y, szerokość, wysokość
+    elipsa(x_middle + 50, y_eye + 20, 60, 50)
+    elipsa(x_middle, y_mouth - 30, 100, 65)
 
 
 def setup():
-    # Put code to run once here
-    size(400, 400)  # width and height
-    background(0, 0, 0)  # move under draw() to reset the drawing every frame
+    # Wstaw kod, aby uruchomić go raz tutaj
+    rozmiar(400, 400) # szerokość i wysokość
+    background(0, 0, 0) # przesuń pod draw(), aby zresetować rysunek każdą klatkę
     rect_mode(CENTER)
     no_stroke()
 
 
 def draw():
-    # Put code to run every frame here
+    # Wstaw kod, aby uruchomić każdą klatkę tutaj
     mask_width = width / 2
-    x_middle = width / 2
-    y_eye = 180
-    y_mouth = 255
-    # draw mask
-    fill(255, 255, 255)  # white
+    x_middle = szerokość / 2
+    y_oko = 180
+    y_usta = 255
+    # narysuj maskę
+    fill(255, 255, 255) # biały
     rect(200, 150, mask_width, mask_width)
-    ellipse(x_middle, 250, mask_width, 140)
-    # eyes and mouth
-    fill(0)  # black
-    ellipse(x_middle - 50, y_eye, 60, 50)  # x, y, width, height
-    ellipse(x_middle + 50, y_eye, 60, 50)
-    ellipse(x_middle, y_mouth, 100, 75)
-    # partly cover eyes and mouth
+    elipsa(x_middle, 250, mask_width, 140)
+    # oczy i usta
+    fill(0) # czarny
+    elipsa(x_middle - 50, y_eye, 60, 50) # x, y, szerokość, wysokość
+    elipsa(x_middle + 50, y_eye, 60, 50)
+    elipsa(x_middle, y_mouth, 100, 75)
+    # częściowo zakryj oczy i usta
     fill(255)
     if mouse_x > x_middle:
-        happy(x_middle, y_eye, y_mouth)
+        szczęśliwy(x_middle, y_eye, y_mouth)
     else:
         sad(x_middle, y_eye, y_mouth)
-    # cover top of mask
+    # przykryj górną część maski
     fill(0)
-    ellipse(x_middle, 60, 250, 90)
-    # shade half of the mask
+    elipsa(x_middle, 60, 250, 90)
+    # przyciemnij połowę maski
     fill(0, 25)
     rect(300, 200, width/2, height)
 
 
 def mouse_pressed():
-    # Put code to run when the mouse is pressed here
-    print(mouse_x, mouse_y)
+    # Wprowadź kod, który będzie uruchamiany po naciśnięciu myszy
+    print(mysz_x, mysz_y)
 
 
-# Keep this to run your code
+# Zatrzymaj to, aby uruchomić swój kod
 run()
