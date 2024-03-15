9c3bacf7890b22a6ddb4495ec98a6e4596c3d646
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-15 13:12:35 +0000

    New translations main.py (French)

--- fr-FR/code/make_a_face_kawaii_fruit_example/main.py
+++ fr-FR/code/make_a_face_kawaii_fruit_example/main.py
@@ -11,30 +11,30 @@ def setup():
 def draw():
     background(255)
     orange = Color(255, 165, 0)
-    brown = Color(200, 120, 0)
-    green = Color(100, 155, 0)
+    brun = Color(200, 120, 0)
+    vert = Color(100, 155, 0)
     fill(orange)
     ellipse(200, 200, 200, 190)
     fill(0)
-    # Eyes
+    # Les yeux
     ellipse(160, 220, 30, 30)
     ellipse(240, 220, 30, 30)
     fill(255)
     ellipse(165, 215, 10, 10)
     ellipse(245, 215, 10, 10)
-    # Mouth
+    # La bouche
     fill(0)
     ellipse(200, 240, 15, 15)
     fill(orange)
     ellipse(200, 235, 15, 15)
-    # Highlights
+    # Zones claires
     fill(255, 70)
     ellipse(170, 150, 35, 35)
     ellipse(150, 160, 25, 25)
-    # stalk
-    fill(brown)
+    # tige
+    fill(brun)
     triangle(200, 130, 220, 60, 240, 60)
-    fill(green)
+    fill(vert)
     translate(180, 85)
     rotate(radians(45))
     ellipse(0, 0, 75, 35)
