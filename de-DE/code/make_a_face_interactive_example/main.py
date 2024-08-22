aef67e86b45d895cd5d8fdaad615bd445c7318ab
ZeljkaNovakBaxter <zeljka.novak.baxter@raspberrypi.org>
2024-08-22 19:53:27 +0100

    New translations main.py (German)

--- de-DE/code/make_a_face_interactive_example/main.py
+++ de-DE/code/make_a_face_interactive_example/main.py
@@ -4,59 +4,59 @@ from p5 import *
 from random import randint
 
 
-def sad(x_middle, y_eye, y_mouth):
-    ellipse(x_middle - 50, y_eye - 20, 60, 50)  # x, y, width, height
-    ellipse(x_middle + 50, y_eye - 20, 60, 50)
-    ellipse(x_middle, y_mouth + 30, 100, 65)
+def traurig(x_mitte, y_auge, y_mund):
+    ellipse(x_mitte - 50, y_auge - 20, 60, 50)  # x, y, Breite, Höhe
+    ellipse(x_mitte + 50, y_auge - 20, 60, 50)
+    ellipse(x_mitte, y_mund + 30, 100, 65)
 
 
-def happy(x_middle, y_eye, y_mouth):
-    ellipse(x_middle - 50, y_eye + 20, 60, 50)  # x, y, width, height
-    ellipse(x_middle + 50, y_eye + 20, 60, 50)
-    ellipse(x_middle, y_mouth - 30, 100, 65)
+def gluecklich(x_mitte, y_auge, y_mund):
+    ellipse(x_mitte - 50, y_auge + 20, 60, 50)  # x, y, Breite, Höhe
+    ellipse(x_mitte + 50, y_auge + 20, 60, 50)
+    ellipse(x_mitte, y_mund - 30, 100, 65)
 
 
 def setup():
-    # Put code to run once here
-    size(400, 400)  # width and height
-    background(0, 0, 0)  # move under draw() to reset the drawing every frame
+    # Code, der hier steht, wird einmal ausgeführt
+    size(400, 400)  # Breite und Höhe
+    background(0, 0, 0)  # verschiebe unter draw(), um die Zeichnung jeden Frame zurückzusetzen
     rect_mode(CENTER)
     no_stroke()
 
 
 def draw():
-    # Put code to run every frame here
-    mask_width = width / 2
-    x_middle = width / 2
-    y_eye = 180
-    y_mouth = 255
-    # draw mask
-    fill(255, 255, 255)  # white
-    rect(200, 150, mask_width, mask_width)
-    ellipse(x_middle, 250, mask_width, 140)
-    # eyes and mouth
-    fill(0)  # black
-    ellipse(x_middle - 50, y_eye, 60, 50)  # x, y, width, height
-    ellipse(x_middle + 50, y_eye, 60, 50)
-    ellipse(x_middle, y_mouth, 100, 75)
-    # partly cover eyes and mouth
+    # Code, der hier steht, wird jeden Frame ausgeführt
+    masken_breite = width / 2
+    x_mitte = width / 2
+    y_auge = 180
+    y_mund = 255
+    # Maske zeichnen
+    fill(255, 255, 255)  # Weiß
+    rect(200, 150, masken_breite, masken_breite)
+    ellipse(x_mitte, 250, masken_breite, 140)
+    # Augen und Mund
+    fill(0)  # Schwarz
+    ellipse(x_mitte - 50, y_auge, 60, 50)  # x, y, Breite, height
+    ellipse(x_mitte + 50, y_auge, 60, 50)
+    ellipse(x_mitte, y_mund, 100, 75)
+    # Augen und Mund teilweise abdecken
     fill(255)
-    if mouse_x > x_middle:
-        happy(x_middle, y_eye, y_mouth)
+    if mouse_x > x_mitte:
+        gluecklich(x_mitte, y_auge, y_mund)
     else:
-        sad(x_middle, y_eye, y_mouth)
-    # cover top of mask
+        traurig(x_mitte, y_auge, y_mund)
+    # oberen Teil der Maske abdecken
     fill(0)
-    ellipse(x_middle, 60, 250, 90)
-    # shade half of the mask
+    ellipse(x_mitte, 60, 250, 90)
+    # Hälfte der Maske schattieren
     fill(0, 25)
     rect(300, 200, width/2, height)
 
 
 def mouse_pressed():
-    # Put code to run when the mouse is pressed here
+    # Dieser Code wird ausgeführt, wenn die Maus gedrückt wird
     print(mouse_x, mouse_y)
 
 
-# Keep this to run your code
+# Lass dies so stehen, um deinen Code auszuführen
 run()
