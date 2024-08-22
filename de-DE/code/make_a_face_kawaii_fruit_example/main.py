af0062b4a1181244e749efdbb4104b51d130d0bd
ZeljkaNovakBaxter <zeljka.novak.baxter@raspberrypi.org>
2024-08-22 19:53:30 +0100

    New translations main.py (German)

--- de-DE/code/make_a_face_kawaii_fruit_example/main.py
+++ de-DE/code/make_a_face_kawaii_fruit_example/main.py
@@ -11,18 +11,18 @@ def setup():
 def draw():
     background(255)
     orange = Color(255, 165, 0)
-    brown = Color(200, 120, 0)
-    green = Color(100, 155, 0)
+    braun = Color(200, 120, 0)
+    gruen = Color(100, 155, 0)
     fill(orange)
     ellipse(200, 200, 200, 190)
     fill(0)
-    # Eyes
+    # Augen
     ellipse(160, 220, 30, 30)
     ellipse(240, 220, 30, 30)
     fill(255)
     ellipse(165, 215, 10, 10)
     ellipse(245, 215, 10, 10)
-    # Mouth
+    # Mund
     fill(0)
     ellipse(200, 240, 15, 15)
     fill(orange)
@@ -32,9 +32,9 @@ def draw():
     ellipse(170, 150, 35, 35)
     ellipse(150, 160, 25, 25)
     # stalk
-    fill(brown)
+    fill(braun)
     triangle(200, 130, 220, 60, 240, 60)
-    fill(green)
+    fill(gruen)
     translate(180, 85)
     rotate(radians(45))
     ellipse(0, 0, 75, 35)
