7dcd354c93a6d7e27271be29efe7fe637f36bb16
ZeljkaNovakBaxter <zeljka.novak.baxter@raspberrypi.org>
2024-09-13 21:24:43 +0100

    New translations main.py (Italian)

--- it-IT/code/make_a_face_stacked_faces_example/main.py
+++ it-IT/code/make_a_face_stacked_faces_example/main.py
@@ -18,19 +18,19 @@ def draw():
     grey = Color(201, 201, 201)
     lilac = Color(160, 158, 214)
 
-    # Sfondo del viso
+    # Sfondo della faccia superiore
     fill(blue)
     rect(50, 100, 300, 200)
     fill(0)
 
-    # Capelli sulla parte superiore del viso
+    # Capelli della faccia superiore
     fill(purple)
     gap = 0
     for i in range(0, 5):
         triangle(100+gap, 140, 120+gap, 120, 140+gap, 140)
         gap = gap+40
 
-    # Faccia occhio sinistro
+    # Occhio sinistro della faccia superiore
     fill(grey)
     rect(80, 190, 100, 50)
     fill(red)
@@ -40,7 +40,7 @@ def draw():
     fill(lilac)
     ellipse(160, 200, 30, 30)
 
-    # Faccia occhio destro
+    # Occhio destro della faccia superiore
     fill(grey)
     rect(220, 190, 100, 50)
     fill(red)
@@ -50,25 +50,25 @@ def draw():
     fill(lilac)
     ellipse(240, 200, 30, 30)
 
-    # Bocca
+    # Bocca della faccia superiore
     fill(brown)
     rect(100, 220, 200, 50)
     fill(purple)
     rect(110, 240, 180, 10)
 
-  # Sfondo del viso parte inferiore
+  # Sfondo della faccia inferiore
     fill(purple)
     rect(50, 300, 300, 200)
     fill(0)
 
-    # Capelli sul viso parte inferiore
+    # Capelli della faccia inferiore
     fill(green)
     gap = 0
     for i in range(0, 5):
         triangle(100+gap, 340, 120+gap, 320, 140+gap, 340)
         gap = gap+40
 
-    # Faccia inferiore Occhio sinistro
+    # Occhio sinistro della faccia inferiore
     fill(red)
     rect(80, 390, 100, 50)
     fill(lilac)
@@ -78,7 +78,7 @@ def draw():
     fill(purple)
     ellipse(160, 400, 30, 30)
 
-    # Faccia inferiore Occhio destro
+    # Occhio destro della faccia inferiore
     fill(red)
     rect(220, 390, 100, 50)
     fill(lilac)
@@ -88,7 +88,7 @@ def draw():
     fill(purple)
     ellipse(240, 400, 30, 30)
 
-    # bocca parte inferiore viso 
+    # Bocca della faccia inferiore
     fill(green)
     rect(100, 420, 200, 50)
     fill(red)
