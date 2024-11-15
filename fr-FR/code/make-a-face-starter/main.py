ced3fb07216bf75c6a2e3ddb35fabdb36b1f71a9
ZeljkaNovakBaxter <zeljka.novak.baxter@raspberrypi.org>
2024-11-15 09:29:21 +0000

    New translations main.py (French)

--- fr-FR/code/make-a-face-starter/main.py
+++ fr-FR/code/make-a-face-starter/main.py
@@ -1,20 +1,18 @@
-#!/bin/python3
-
 from p5 import *
-from grid import *
+
+screen_size = 400
 
 
 def setup():
     # Mets le code à exécuter une fois ici
-    size(400, 400) # largeur et hauteur
+    size(screen_size, screen_size)
+    rect_mode(CENTER)
 
 
 def draw():
     # Mets le code à exécuter à chaque image ici
-    background(255, 255, 255)
+    background(0, 0, 0)
     # Ajoute du code pour dessiner ton visage ici
-
-    grid() # ajoute un # au début de cette ligne pour masquer la grille
 
 
 # Garde ceci pour exécuter ton code
