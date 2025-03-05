94406d0ccea6ddbb7e97bbcd6095ffdb78fe0c33
ZeljkaNovakBaxter <zeljka.novak.baxter@raspberrypi.org>
2025-03-05 18:10:32 +0000

    New translations main.py (German)

--- de-DE/code/make_a_face_stacked_faces_example/main.py
+++ de-DE/code/make_a_face_stacked_faces_example/main.py
@@ -16,79 +16,79 @@ def draw():
     lila = Color(75, 37, 109)
     braun = Color(178, 162, 150)
     grau = Color(201, 201, 201)
-    helllila = Color(160, 158, 214)
+    flieder = Color(160, 158, 214)
 
-    # Top face background
+    # Hintergrund des oberen Gesichts
     fill(blau)
     rect(50, 100, 300, 200)
     fill(0)
 
-    # Top face Hair
-    fill(helllila)
+    # Haare im oberen Gesicht
+    fill(flieder)
     luecke = 0
     for i in range(0, 5):
         triangle(100+luecke, 140, 120+luecke, 120, 140+luecke, 140)
         luecke = luecke+40
 
-    # Top face Left Eye
+    # Oberes Gesicht, linkes Auge
     fill(gruen)
     rect(80, 190, 100, 50)
     fill(rot)
     triangle(190, 250, 70, 150, 180, 160)
     fill(gruen)
     triangle(190, 250, 60, 160, 180, 170)
-    fill(lila)
+    fill(flieder)
     ellipse(160, 200, 30, 30)
 
-    # Top face Right Eye
+    # Oberes Gesicht, rechtes Auge
     fill(gruen)
     rect(220, 190, 100, 50)
     fill(rot)
     triangle(210, 250, 330, 150, 220, 160)
-    fill(helllila)
+    fill(gruen)
     triangle(210, 250, 340, 160, 220, 170)
-    fill(lila)
+    fill(flieder)
     ellipse(240, 200, 30, 30)
 
-    # Top face Mouth
+    # Oberes Gesicht, Mund
     fill(braun)
     rect(100, 220, 200, 50)
-    fill(helllila)
+    fill(lila)
     rect(110, 240, 180, 10)
 
-  # Bottom face background
-    fill(grau)
+  # Hintergrund des unteren Gesichts
+    fill(lila)
     rect(50, 300, 300, 200)
     fill(0)
 
-    # Bottom face Hair
-    fill(lila)
+    # Haare im unteren Gesicht
+    fill(gruen)
     luecke = 0
     for i in range(0, 5):
         triangle(100+luecke, 340, 120+luecke, 320, 140+luecke, 340)
         luecke = luecke+40
 
-    # Bottom face Left Eye
+    # Unteres Gesicht, linkes Auge
     fill(rot)
     rect(80, 390, 100, 50)
-    fill(lila)
+    fill(flieder)
     triangle(190, 450, 70, 350, 180, 360)
     fill(braun)
     triangle(190, 450, 60, 360, 180, 370)
-    fill(grau)
+    fill(lila)
     ellipse(160, 400, 30, 30)
 
-    # Bottom face Right Eye
+    # Unteres Gesicht, rechtes Auge
     fill(rot)
     rect(220, 390, 100, 50)
-    fill(lila)
+    fill(flieder)
     triangle(210, 450, 330, 350, 220, 360)
     fill(braun)
     triangle(210, 450, 340, 360, 220, 370)
-    fill(helllila)
+    fill(lila)
     ellipse(240, 400, 30, 30)
 
-    # Bottom face Mouth
+    # Unteres Gesicht, Mund
     fill(gruen)
     rect(100, 420, 200, 50)
     fill(rot)
